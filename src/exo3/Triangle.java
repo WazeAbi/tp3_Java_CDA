05c779eec872a89be1e71ba12fa63b3dbcec9b7c
WazeAbi <alexdalle12@gmail.com>
2024-02-08 23:46:07 +0100

    Build(Triangle):Création de la classe et des getter&setter

---
+++ src/exo3/Triangle.java
@@ -0,0 +1,52 @@
+package exo3;
+
+import exo2.Forme;
+
+public class Triangle extends Forme {
+    private float side_1;
+    private float side_2;
+    private float side_3;
+    private float base;
+    private float height;
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getSide_1() {
+        return side_1;
+    }
+
+    public void setSide_1(float side_1) {
+        this.side_1 = side_1;
+    }
+
+    public float getSide_2() {
+        return side_2;
+    }
+
+    public void setSide_2(float side_2) {
+        this.side_2 = side_2;
+    }
+
+    public float getSide_3() {
+        return side_3;
+    }
+
+
+    }
+
+
+}
