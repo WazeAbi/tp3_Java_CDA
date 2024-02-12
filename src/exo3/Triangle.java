0ab01c217f62f278039b3783a988c0b4f4161348
WazeAbi <alexdalle12@gmail.com>
2024-02-12 12:21:37 +0100

    Build(Exo3 et Exo4): Création Methode Triangle et Création Interface Forme

--- src/exo3/Triangle.java
+++ src/exo3/Triangle.java
@@ -3,50 +3,59 @@ package exo3;
 import exo2.Forme;
 
 public class Triangle extends Forme {
-    private float side_1;
-    private float side_2;
-    private float side_3;
-    private float base;
-    private float height;
+    private double side_1;
+    private double side_2;
+    private double side_3;
+    private double base;
+    private double height;
 
-    public float getBase() {
-        return base;
+    public double perimetre() {
+        return side_1+side_2+side_3;
     }
-
-    public void setBase(float base) {
-        this.base = base;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public float getSide_1() {
+    public double getSide_1() {
         return side_1;
     }
 
-    public void setSide_1(float side_1) {
+    public void setSide_1(double side_1) {
         this.side_1 = side_1;
     }
 
-    public float getSide_2() {
+    public double getSide_2() {
         return side_2;
     }
 
-    public void setSide_2(float side_2) {
+    public void setSide_2(double side_2) {
         this.side_2 = side_2;
     }
 
-    public float getSide_3() {
+    public double getSide_3() {
         return side_3;
     }
 
+    public void setSide_3(double side_3) {
+        this.side_3 = side_3;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
 
     }
+
+
 
 
 }
